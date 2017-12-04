e4d2e74308067801b9bf1ac8217d888c7d060485
William Raimundo Azevedo <willianray@gmail.com>
2017-12-04 21:58:50 -0200

    Part 1, Lesson 6 testdrive.io

--- manage.py
+++ manage.py
@@ -3,17 +3,12 @@
 import unittest
 
 from flask_script import Manager
-from project import app, db
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
 
 manager = Manager(app)
-
-
-@manager.command
-def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 @manager.command
@@ -25,6 +20,20 @@ def test():
         return 0
     return 1
 
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email='michael@realpython.com'))
+    db.session.add(User(username='michaelherman', email='michael@mherman.org'))
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
